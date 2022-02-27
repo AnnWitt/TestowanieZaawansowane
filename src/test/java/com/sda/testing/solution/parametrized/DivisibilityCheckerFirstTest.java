52edb76ae34d404072c090b298a8ae983507a32e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 09:57:07 +0100

    -- first test merge z trenerem

--- src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerFirstTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerFirstTest.java
@@ -1,6 +1,5 @@
 package com.sda.testing.solution.parametrized;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
