8029c7e978164ca6add4219e3d94c5d354579d23
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 10:08:47 +0100

    -- DivisibilityCheckerTest first parametrized test

--- src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerTest.java
@@ -1,10 +1,9 @@
 package com.sda.testing.solution.parametrized;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class DivisibilityCheckerTest {
 
@@ -15,25 +14,6 @@ class DivisibilityCheckerTest {
         boolean actual = DivisibilityChecker.isDivisibleBy3(number);
         //then
         assertTrue(actual);
-    }
-
-    @ParameterizedTest(name = "run #{index} with [{arguments{]")// po kolei rozne wartosci
-    @ValueSource(ints = {-3,-6,0,3,9,12,15,10,7}) //załatwia given
-    void shouldNotBeDivisibleBy3(int number) {
-        //when
-        boolean actual = DivisibilityChecker.isDivisibleBy3(number);
-        //then
-        assertFalse(actual);
-    }
-
-    @ParameterizedTest
-    @CsvSource({"0,true","1,false", "-3,true","3,false","-8, false"})
-    void shouldCheckDivisibility(int number,boolean expected) {
-        //when
-        boolean actual = DivisibilityChecker.isDivisibleBy3(number);
-        //then
-       // assertEquals(np 7,actual);
-        assertEquals(expected,actual);
     }
 
 }
